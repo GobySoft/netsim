557990d0d027608834364b709740e0a1788e1bbf
Toby Schneider <toby@gobysoft.org>
2020-11-09 16:55:33 +0000

    Add scripts and option for loopback testing

---
+++ scripts/restart.sh
@@ -0,0 +1,8 @@
+sudo systemctl daemon-reload
+sudo systemctl stop jackd
+sleep 2
+sudo systemctl start jackd
+sudo systemctl restart netsim_core netsim_manager gobyd goby_logger netsim_socat@{0..3} netsim_postprocess netsim_liaison
+sudo systemctl restart netsim_moos-atlantic netsim_moos_gateway-atlantic
+sleep 2
+sudo systemctl restart netsim_moos-arctic netsim_moos_gateway-arctic 
