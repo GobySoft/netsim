264343a7380f1b297588db34ff7ea0bfb6d7744f
Toby Schneider <toby@gobysoft.org>
2022-08-29 07:19:51 +0000

    Updates for iris

--- scripts/status.sh
+++ scripts/status.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-systemctl status netsim_core goby_logger netsim_manager netsim_moos-atlantic netsim_moos_gateway-atlantic netsim_moos-arctic netsim_moos_gateway-arctic gobyd jackd netsim_socat@{0..7} netsim_postprocess netsim_liaison
+systemctl list-units -all "netsim*" "goby*" "jack*"
 
